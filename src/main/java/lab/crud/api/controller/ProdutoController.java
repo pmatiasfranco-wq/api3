1b6f3cd5e692f19e0bf14d878e41f14ad9b5e678
pmatiasfranco-wq <pmatiasfranco@gmail.com>
2025-10-01 22:29:23 -0300

    Add Spring Data JPA e ProdutoRepository

--- src/main/java/lab/crud/api/controller/ProdutoController.java
+++ src/main/java/lab/crud/api/controller/ProdutoController.java
@@ -1,30 +1,50 @@
 package lab.crud.api.controller;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lab.crud.api.model.Produto;
+import lab.crud.api.repository.ProdutoRepository;
 
 @RestController
 public class ProdutoController {
 
-	
-	//curl -X POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
+	@Autowired
+	 private ProdutoRepository repository; 
+	 
+	 //curl -X POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
 	
 	//@RequestMapping(method = RequestMethod.POST, path = "/produto")
 	@PostMapping("/produtos")
 	public ResponseEntity<Produto> novo(
 			@RequestBody Produto produto) {
 		
-		System.out.println("nome:" + produto.getNome());
-		System.out.println("mês:" + produto.getDataCriacao().getMonthValue());
+		
+		produto.setDataCriacao(LocalDate.now());
+		
+	repository.save(produto);
+		
+		System.out.println(produto.toString());
+		
+			
 		
 		return ResponseEntity
-				.status(HttpStatus.NOT_FOUND)
+				.status(HttpStatus.CREATED)
 				.body(produto);
 	}
+	@GetMapping("/produtos")
+	public ResponseEntity<Iterable<Produto>> obterTodos(){
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(repository.findAll());
+	}
 	
+ 
 }
---
+++ src/main/java/lab/crud/api/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package lab.crud.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lab.crud.api.model.Produto;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+
+}
